a24fbf96a03e35e3067508374024c4063be69b0f
HEMANT CHANDRA <119705728+hemant-chandra@users.noreply.github.com>
2025-09-18 23:26:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let todo = [];
+req = prompt("Please Enter your Request...");
+while(true){
+    if(req=="quit"){
+        console.log("Quiting ToDo...");
+        break;
+    }
+    if(req=="list") {
+        console.log("---------------------");
+        for(let i=0; i<todo.length; i++){
+            console.log(i, todo[i]);
+        }
+        console.log("---------------------");
+    } else if(req=="add"){
+        let task = prompt("Please Enter the task you want to add : ");
+        todo.push(task);
+        console.log("Task Added");
+    } else if (req=="delete") {
+        let idx = prompt("Please enter the task index to delete :");
+        todo.splice(idx, 1);
+        console.log("Task Deleted...");
+    } else {
+        console.log("Wrong Request.....");
+    }
+    req = prompt("Please Enter your Request...");
+}
